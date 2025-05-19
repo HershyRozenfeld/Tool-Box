daef53003930831eb8807ea6f27a30d4934605e3
hershy rozenfeld <Y6764057@gmail.com>
2025-05-19 12:39:26 +0300

    Add project files.

---
+++ Tool.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace toolbox
+{
+    internal class Tool
+    {
+        private string _name;
+        private int _weight;
+        // private bool use = false;
+        public void Describe()
+        {
+
+        }
+        public void Use()
+        {
+
+        }
+    }
+}
