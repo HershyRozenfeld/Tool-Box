c21d5461b2b402ac4b558236034bc3e74a6daa8e
hershy rozenfeld <Y6764057@gmail.com>
2025-05-19 15:24:40 +0300

    finished task 3

--- Program.cs
+++ Program.cs
@@ -11,7 +11,15 @@ namespace toolbox
         static void Main(string[] args)
         {
             Hammer hammer = new Hammer();
+            PipeCutter pipeCutter_1 = new PipeCutter();
+            PipeCutter pipeCutter_2 = new PipeCutter();
+            Screwdriver screwdriver = new Screwdriver();
+            Saw saw = new Saw();
             hammer.Use();
+
+            Tool[] tools = new Tool[] { hammer, pipeCutter_1, pipeCutter_2, screwdriver, saw };
+            
+
         }
     }
 }
